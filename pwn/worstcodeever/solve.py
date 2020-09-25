6d1433a81d5ab8b8e8490da4e34f3625e29ae928
Adrian Self <aself.school@gmail.com>
2020-09-24 22:50:01 -0400

    start working on worstcodeever

--- pwn/worstcodeever/solve.py
+++ pwn/worstcodeever/solve.py
@@ -2,35 +2,54 @@
 
 from pwn import *
 
-conn = remote('pwn.red.csaw.io', 5008)
+if args.REMOTE:
+    conn = remote('pwn.red.csaw.io',5008)
+else:
+    conn = gdb.debug('./worstcodeever', gdbscript=(''
+         +'break display\n'
+         +'commands\n'
+         +'heap bins\n'#unfortunately this shows up before gef context
+         +'end\n'
+         +'c\n'#start automatically
+        )
+    )
 
-def createCharacter(name):
-    assert len(name) <= 15
-    print(conn.recv().decode())
-    conn.sendline(b'1')
-    print(conn.recv().decode())
-    conn.sendline(name)
+""" Notes
+sizeof(struct Friend) == 16
+sizeof(union identifier) == 8
+tcache is used when friends are freed
+"""
 
-def viewCharacter(index):
-    print(conn.recv().decode())
-    conn.sendline(b'2')
-    print(conn.recv().decode())
-    conn.sendline(str(index).encode())
+def addfriend():#TODO params
+    pass#TODO
+    #set option to 1
+    #set int human_name
+    #if human_name != 0:
+        #set char[63] name via fgets; send all 63 or use \n which will be replaced with \0 to end
+    #else:
+        #set int64 id
+    #set int age
 
-def renameCharacter(index, name):
-    assert len(name) <= 15
-    print(conn.recv().decode())
-    conn.sendline(b'3')
-    print(conn.recv().decode())
-    conn.sendline(str(index).encode())
-    print(conn.recv().decode())
-    conn.sendline(name)
+def removefriend():#TODO params
+    pass#TODO
+    #set option to 2
+    #set index
 
-#createCharacter(b'freddy')
-#viewCharacter(0)
-#renameCharacter(0,b'mercury')
-#viewCharacter(0)
+def editfriend():#TODO params
+    pass#TODO
+    #set option to 4
+    #set index
+    #if friend_type[index] != 0:
+        #set char[63] name via fgets; send all 63 or use \n which will not be replaced but \0 will be tacked on
+    #else:
+        #set int64 id
+    #set int age
 
+def display():#TODO params
+    pass#TODO
+    #set option to 3
+    #set index
+    
 conn.interactive()
 
 conn.close()
