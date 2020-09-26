66785cdf9103d9fdd41aee21d157699a86db92a3
Adrian Self <aself.school@gmail.com>
2020-09-26 12:19:09 -0400

    finish crypto

--- crypto/prettyplease/solve.py
+++ crypto/prettyplease/solve.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 from pwn import *
+from base64 import b64decode, b64encode
+
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+# encrypts b'Your application has been REJECTED'
+# using a random IV in CTR mode
+def get_ct():
+    conn.sendlineafter('> ','1')
+    conn.sendlineafter('> ','')
+    conn.recvuntil('Token: ')
+    token = b64decode(conn.recvuntil('\r\n').strip())
+    iv = token[:16]
+    ct = token[16:]
+    return iv, ct
+
+def process(msg):
+    msg = b64encode(msg)
+    conn.sendlineafter('> ','2')
+    conn.sendlineafter('> ',msg)
+
+given  = b'Your application has been REJECTED'
+target = b'Your application has been ACCEPTED'
+difference = byte_xor(given, target)
 
 conn = remote('crypto.red.csaw.io',5012)
 
-print(conn.recv())
+iv, ct = get_ct()
 
-#Solve chall here
+msg = iv + byte_xor(ct,difference)
 
-conn.interactive()
+process(msg)
+
+print(conn.recvall().decode())
 
 conn.close()
